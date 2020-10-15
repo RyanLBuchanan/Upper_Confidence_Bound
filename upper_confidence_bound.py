0c52bd0c064e7295152419d31231d4b728fd2a4e
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-15 10:43:31 -0600

    Updated Jupyter

--- upper_confidence_bound.py
+++ upper_confidence_bound.py
@@ -1,10 +1,12 @@
 # Upper Confidence Bound Algorithm tutorial from Machine Learning A-Z - SuperDataScience -> Input by Ryan L Buchanan 15OCT20
 
 # Import the libraries
-
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
 # Import the dataset
-
+dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
 
 # Implement the Upper Confidence Bound algorithm
 
